e3e02d49df2e1efbcf445d764652a7738320b021
BhatiKaran <karanbhatia198@gmail.com>
2023-01-18 17:02:56 +0530

    change

--- dev/TestProgram.py
+++ dev/TestProgram.py
@@ -7,17 +7,22 @@
 #     f += 1
 # for i in countries:
 #     print(i)
-password = input("enter ur password: ")
-i = ['1', '2', '3', '4', '5', '!', '@', '#', '$', '%', 'a', 'b', 'c']
-if len(password) >= 8:
-    for letter in password:
-        if letter not in i:
-            print("pw not in range")
-            break
-    else:
-        print("with in range")
-else:
-    print("password must be more than 8 Characters")
+# password = input("enter ur password: ")
+# i = ['1', '2', '3', '4', '5', '!', '@', '#', '$', '%', 'a', 'b', 'c']
+# if len(password) >= 8:
+#     for letter in password:
+#         if letter not in i:
+#             print("pw not in range")
+#             break
+#     else:
+#         print("with in range")
+# else:
+#     print("password must be more than 8 Characters")
+
+dictionary = {'name': 'jaggu','rollno': '20'}
+print(dictionary)
+
+
 
 
 
