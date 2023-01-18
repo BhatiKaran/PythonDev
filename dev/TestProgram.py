d88889cbcbf05dff1e6f167c479758ef303f1eab
BhatiKaran <karanbhatia198@gmail.com>
2023-01-18 16:30:19 +0530

    change

---
+++ dev/TestProgram.py
@@ -0,0 +1,27 @@
+# countries = []
+# f = 0
+# while f < 10:
+#     x = input("please give name : ")
+#     countries.append(x)
+#     print(countries)
+#     f += 1
+# for i in countries:
+#     print(i)
+password = input("enter ur password: ")
+i = ['1', '2', '3', '4', '5', '!', '@', '#', '$', '%', 'a', 'b', 'c']
+if len(password) >= 8:
+    for letter in password:
+        if letter not in i:
+            print("pw not in range")
+            break
+    else:
+        print("with in range")
+else:
+    print("password must be more than 8 Characters")
+
+
+
+
+
+
+
